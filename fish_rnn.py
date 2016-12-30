2c30b897bb63f8ed23ecdc6353bf1c6507a74a96
Vinod Rajendran <vinodr.msec@gmail.com>
2016-12-30 18:09:00 +0800

    RNN with minimal parameter setting in order to run in 1 available GPU

--- fish_rnn.py
+++ fish_rnn.py
@@ -92,35 +92,7 @@ print X_valid.shape
 def center_normalize(x):
     return (x - K.mean(x)) / K.std(x)
 
-# Build CNN network
-'''
-model = Sequential()
-model.add(Activation(activation=center_normalize, input_shape=(ROWS, COLS, CHANNELS)))
-model.add(Convolution2D(32, 3, 3, border_mode='same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Convolution2D(32, 3, 3, border_mode='same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Convolution2D(64, 3, 3, border_mode='same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(8))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-'''
 # Build LSTM network
 model = Sequential()
 model.add(Activation(activation=center_normalize, input_shape=X_train.shape[1:]))
